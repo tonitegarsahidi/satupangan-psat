5899f82e746b46c5d70fb61159cf321fd1f248b7
toni tegar sahidi <tonitegarsahidi@gmail.com>
2025-08-17 14:02:14 +0700

    update: batas cemaran seeder

--- database/seeders/BatasCemaranSeeder.php
+++ database/seeders/BatasCemaranSeeder.php
@@ -42,8 +42,12 @@ class BatasCemaranSeeder extends Seeder
                     'id' => Str::uuid(),
                     'jenis_psat' => $jenisPangan->id,
                     'cemaran_logam_berat' => $logamBerat->id,
-                    'value_min' => rand(1, 50) / 10,
-                    'value_max' => rand(50, 200) / 10,
+                    'value_min' => 0,
+                    'value_max' => match($logamBerat->kode_cemaran_logam_berat) {
+                        'CLB001' => 0.3, // Timbal (Pb)
+                        'CLB002' => 0.2, // Kadmium (Cd)
+                        default => 0.1
+                    },
                     'satuan' => 'mg/kg',
                     'metode' => 'SNI 01-2893-1992',
                     'keterangan' => 'Batas maksimum logam berat untuk ' . $jenisPangan->nama_jenis_pangan_segar,
@@ -62,7 +66,11 @@ class BatasCemaranSeeder extends Seeder
                     'jenis_psat' => $jenisPangan->id,
                     'cemaran_mikroba' => $mikroba->id,
                     'value_min' => 0,
-                    'value_max' => rand(10, 500),
+                    'value_max' => match($mikroba->kode_cemaran_mikroba) {
+                        'CMB001' => 10,   // Escherichia coli
+                        'CMB002' => 0,    // Salmonella sp. (tidak boleh ada)
+                        default => 100
+                    },
                     'satuan' => 'CFU/g',
                     'metode' => 'SNI 01-2892-1992',
                     'keterangan' => 'Batas maksimum mikroba untuk ' . $jenisPangan->nama_jenis_pangan_segar,
@@ -81,7 +89,11 @@ class BatasCemaranSeeder extends Seeder
                     'jenis_psat' => $jenisPangan->id,
                     'cemaran_mikrotoksin' => $mikrotoksin->id,
                     'value_min' => 0,
-                    'value_max' => rand(1, 50) / 100,
+                    'value_max' => match($mikrotoksin->kode_cemaran_mikrotoksin) {
+                        'CMT001' => 0.025, // Aflatoksin
+                        'CMT002' => 0.05,  // Okratoksin A
+                        default => 0.01
+                    },
                     'satuan' => 'mg/kg',
                     'metode' => 'SNI 01-7208-2016',
                     'keterangan' => 'Batas maksimum mikrotoksin untuk ' . $jenisPangan->nama_jenis_pangan_segar,
@@ -100,7 +112,11 @@ class BatasCemaranSeeder extends Seeder
                     'jenis_psat' => $jenisPangan->id,
                     'cemaran_pestisida' => $pestisida->id,
                     'value_min' => 0,
-                    'value_max' => rand(1, 10) / 100,
+                    'value_max' => match($pestisida->kode_cemaran_pestisida) {
+                        'PST001' => 0.01, // Karbofuran
+                        'PST002' => 0.05, // Klorpirifos
+                        default => 0.01
+                    },
                     'satuan' => 'mg/kg',
                     'metode' => 'SNI 01-3982-2006',
                     'keterangan' => 'Batas maksimum residu pestisida untuk ' . $jenisPangan->nama_jenis_pangan_segar,
